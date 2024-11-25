f3887ff4e3aedea7157224f34dce506b330b669f
Elliewqs <elliemoura10@gmail.com>
2024-11-25 20:28:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Genshin Impact',
+    'O que é Pyro?',
+    'O Pyro é o elemento de fogo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tokyo'
+)
+
+criaCartao(
+    'Musica',
+    'Qual o kpop mais famoso do mundo?',
+    'Gangnam Style'
+)
+
+criaCartao(
+    'Lingua Japonesa',
+    'Como se diz Obrigado em Japonês?',
+    'Obrigado em Japonês é Arigato.'
+)
